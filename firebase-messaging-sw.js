822c92f9efd056b669a8c7a8983d18b4b84fb536
wesendmoney <lucassendmoney@gmail.com>
2025-05-24 21:32:27 -0400

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,20 +1,25 @@
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-firebase.initializeApp({
-  apiKey: "AIzaSyA0NDOIw9wTunNGyJTHgh8JHmMM__hUzrk",
-  authDomain: "wesm-6ce39.firebaseapp.com",
-  projectId: "wesm-6ce39",
-  storageBucket: "wesm-6ce39.appspot.com",
-  messagingSenderId: "417323501500",
-  appId: "1:417323501500:web:2550c12546e7de0f4f8db9"
-});
+const firebaseConfig = {
+    apiKey: "AIzaSyA0NDOIw9wTunNGyJTHgh8JHmMM__hUzrk",
+    authDomain: "wesm-6ce39.firebaseapp.com",
+    projectId: "wesm-6ce39",
+    storageBucket: "wesm-6ce39.appspot.com",
+    messagingSenderId: "417323501500",
+    appId: "1:417323501500:web:2550c12546e7de0f4f8db9",
+    measurementId: "G-H2H6Y2WVSF"
+};
 
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// Manejar notificaciones en segundo plano
 messaging.onBackgroundMessage((payload) => {
-  console.log("Notificación en segundo plano recibida:", payload);
-  const { title, body } = payload.notification;
-  return self.registration.showNotification(title, { body, icon: '/logo.svg' });
+    console.log('Mensaje en segundo plano:', payload);
+    const notificationTitle = payload.notification?.title || 'Nueva notificación';
+    const notificationOptions = {
+        body: payload.notification?.body,
+        icon: '/icon.png' // Asegúrate de tener este icono
+    };
+    return self.registration.showNotification(notificationTitle, notificationOptions);
 });
